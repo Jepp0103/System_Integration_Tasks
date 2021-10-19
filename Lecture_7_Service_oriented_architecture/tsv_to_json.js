f351edd1ef27e33f36f1d6d40a8feaeaec53c587
Jepp0103 <jeppendyekjaer@hotmail.com>
2021-10-19 14:16:07 +0200

    TSV to JSON converter and refactored the whole python virtual environment into one folder

---
+++ Lecture_7_Service_oriented_architecture/tsv_to_json.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+
+axios.get('https://docs.google.com/spreadsheets/d/e/2PACX-1vRYbybiVenyLTJ39ysmw36TfyJWU8p9UK72f81LJ_eIo6vwsV9va_aWETMCgl8EXOefIzTRABU7DqRZ/pub?output=tsv')
+    .then(function (response) {
+        console.log("TSV:")
+        console.log(response.data);
+
+        console.log("JSON:")
+        console.log(tsvToJson(response.data));
+    })
+    .catch(function (error) {
+        console.log(error)
+    });
+
+function tsvToJson(tsv) {
+    const [headers, ...rows] = tsv.trim().split('\n').map(r => r.split('\t'))
+    jsonData = JSON.stringify(rows.reduce((a, r) => [
+        ...a,
+        Object.assign(... (r.map(
+            (x, i, _, c = x.trim()) => ({ [headers[i].trim()]: isNaN(c) ? c : Number(c) })
+        )))
+    ], []));
+
+    return jsonData
+}
